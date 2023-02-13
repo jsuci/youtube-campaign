37620a069e302e89646b39b2064106607c5f407b
jsuci <jsuci.jsuci@gmail.com>
2023-02-14 01:37:47 +0800

    fix getting description and downloading images

--- index.js
+++ index.js
@@ -1,49 +1,121 @@
-const readline = require('readline');
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
-});
 const puppeteer = require('puppeteer');
 const https = require('https');
-const gm = require('gm').subClass({imageMagick: true});
 const fs = require('fs');
 const path = require('path');
+const sharp = require('sharp');
 
-const data = {}
 
-rl.question('Enter appID (ex. com.originatorkids.EndlessAlphabet): ', async (searchTerm) => {
+async function getTitle(page) {
+  const invalidChars = /[\\/:*?"<>|]/g;
+  const spanElement = await page.$('h1 > span');
+  const spanTitle = await page.evaluate(element => element.textContent, spanElement);
+  const appTitle = spanTitle.trim().replace(invalidChars, "");
+  return appTitle;
+}
+
+async function getDescription(page) {
+  const descElement = await page.$('div[data-g-id="description"]');
+  const description = await page.evaluate(element => element.innerHTML , descElement);
+  return description;
+}
+
+async function getThumbnail(page) {
+  const thumbnailElement = await page.$('.l8YSdd > img');
+  const attributeValue = await page.evaluate((el, attr) => el.getAttribute(attr), thumbnailElement, 'src');
+  const thumbnailURL = attributeValue.replace('s48', 's512');
+  return [thumbnailURL];
+}
+
+
+async function getAppImages(page) {
+  return await page.evaluate(() => {
+    const images = Array.from(document.querySelectorAll('div[role="listitem"] img'));
+    return images.map(img => img.getAttribute('src').replace('w526-h296', 'w2560-h1440'));
+  });
+}
+
+
+async function downloadFile(url, path) {
+  return new Promise((resolve, reject) => {
+    https.get(url, (response) => {
+      let downloadedImageBuffer = [];
+      response.on('data', (chunk) => {
+        downloadedImageBuffer.push(chunk);
+      });
+      response.on('end', () => {
+        downloadedImageBuffer = Buffer.concat(downloadedImageBuffer);
+        sharp(downloadedImageBuffer)
+          .resize(1920, 1080, {
+            kernel: sharp.kernel.lanczos3,
+            fit: 'outside',
+          })
+          .webp()
+          .toFile(path, (err, info) => {
+            if (err) {
+              console.error(err);
+            } else {
+              console.log(`Image converted and saved to: ${path}`);
+            }
+          })
+        resolve()
+      });
+      response.on('error', reject);
+    });
+  });
+}
+
+
+async function downloadAppImages(imageList, appTitle, isThumbnail) {
+  const directory = 'apps/' + appTitle;
+  let i = 1;
+
+  if (!fs.existsSync(directory)) {
+    fs.mkdirSync(directory, {recursive: true});
+  }
+
+  for (const imageUrl of imageList) {
+    if (isThumbnail) {
+      fileName = "app-thumbnail.webp"
+    } else {
+      fileName = `${i++}.webp`
+    }
+    const imagePath = path.join(directory, fileName);
+    await downloadFile(imageUrl, imagePath);
+  }
+}
+
+(async () => {
+  const data = {}
+  const searchTerm = process.argv[2];
   const url = `https://play.google.com/store/apps/details?id=${encodeURIComponent(searchTerm.trim())}`;
-
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
 
   await page.goto(url);
   console.log(`Opened page: ${url}\n`);
 
-  const spanElement = await page.$('h1 > span');
-  const spanTitle = await page.evaluate(element => element.textContent, spanElement);
-  const appTitle = spanTitle.trim()
-  data['title'] = appTitle
+  const appTitle = await getTitle(page)
+  data['appTitle'] = appTitle
   console.log(`Extracted title: ${appTitle}`);
 
+  const thumbURL = await getThumbnail(page)
+  data['thumbURL'] = thumbURL
+  console.log(`Extracted thumbnail: ${thumbURL.length}`);
 
-  const element = await page.$('.l8YSdd > img');
-  const attributeValue = await page.evaluate((el, attr) => el.getAttribute(attr), element, 'src');
-  const thumbnailURL = attributeValue.replace('s48', 's512')
-  const directory = 'apps/' + appTitle;
-  const webpPath = path.join(directory, 'app-thumbnail.webp');
+  const appImages = await getAppImages(page)
+  data['appImages'] = appImages
+  console.log(`Extracted appImages: ${appImages.length}\n`);
 
-  if (!fs.existsSync(directory)) {
-    fs.mkdirSync(directory, {recursive: true});
-  }
+  const description = await getDescription(page)
+  data['appDesc'] = description
+  console.log(`Extracted description: ${description.length}\n`);
 
-  https.get(thumbnailURL, (response) => {
-    response.pipe(fs.createWriteStream(webpPath));
-  });
+  console.log(data, '\n')
 
-  data['thumbnailURL'] = thumbnailURL
-  console.log(`Extracted thumbnail: ${thumbnailURL}`);
+  await downloadAppImages(thumbURL, appTitle, true)
+  await downloadAppImages(appImages, appTitle, false)
+
+
 
   await browser.close();
-  rl.close();
-});
+})();
